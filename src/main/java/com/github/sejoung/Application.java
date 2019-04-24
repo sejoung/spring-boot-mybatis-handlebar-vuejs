77acabcfb45ef7af9cd5c4b47466ad505f8c7350
sejoungkim <sejoung@gmail.com>
2019-04-24 12:31:53 +0900

    추가

--- src/main/java/com/github/sejoung/Application.java
+++ src/main/java/com/github/sejoung/Application.java
@@ -7,20 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-  public static void main(String[] args) {
-    //한글[UTF8]처리
-    System.setProperty("file.encoding","UTF-8");
-
-    try {
-      Field charset = Charset.class.getDeclaredField("defaultCharset");
-      charset.setAccessible(true);
-      charset.set(null, null);
-    }catch(Exception e){
-      System.out.println("Charset 오류 : " + e.getStackTrace());
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
-
-    SpringApplication.run(Application.class, args);
-  }
 
 }
